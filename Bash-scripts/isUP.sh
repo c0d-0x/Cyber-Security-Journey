582a18a1c465f873c8fe7a2bb486ca4812acad14
c0d-0x <c0d_0x007@proton.me>
2023-07-22 04:55:59 +0100

    Bash-101

---
+++ Bash-scripts/isUP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+    echo "Enter an IP"
+    echo "Syntax: ./isUP.sh 192.168.0 "
+else
+    for x in $(seq 1 254)
+    do
+         _ip_strg=$( ping -c 1  $1.$x | grep 64 | cut -d ' ' -f 4 | tr -d : )
+        if [ "$_ip_strg" != "" ]; then
+            echo "$_ip_strg is up!!"
+        fi
+    done
+fi
+
+        
