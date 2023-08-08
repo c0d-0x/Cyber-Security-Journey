5016dcc0bac89a97ccd7483fc30495819e39cf61
c0d-0x <c0d_0x007@proton.me>
2023-08-08 03:26:33 +0100

    fix on strip

--- Bash-scripts/strip.sh
+++ Bash-scripts/strip.sh
@@ -3,8 +3,8 @@
 
 for x in $(ls)
 do 
-     typef=$( file x | cut -d ' ' -f 2 )
-     if [ "${typef}" == "PNG" || "${typef}" == "JPEG" ]; then
+     typef=$( file $x | cut -d ' ' -f 2 )
+     if [ "$typef" == "PNG" ] || [ "$typef" == "JPEG" ]; then
         exiftool -all= $x
      fi
 done
